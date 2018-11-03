e462f108b1e8a1648fd9a1026f91753618584871
Kamil Małek <zenit710@gmail.com>
2018-11-03 21:12:35 +0100

    draw board

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,39 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+    private final int boardSize = 10;
+
+    @FXML
+    private GridPane oponentPane;
+
+    @FXML
+    private GridPane playerPane;
+
+    @FXML
+    public void initialize() {
+        initShotButtons();
+    }
+
+    private void initShotButtons()
+    {
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                Button btn = new Button();
+                btn.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        System.out.println(GridPane.getRowIndex(btn));
+                        System.out.println(GridPane.getColumnIndex(btn));
+                    }
+                });
+                oponentPane.add(btn, j, i);
+            }
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +15,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Sea Battle");
+        primaryStage.setScene(new Scene(root, 1000, 400));
         primaryStage.show();
     }
 
